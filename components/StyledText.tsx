304730341213df683e9a27d01e701619ae2b77dc
jonjau <jonjau3q@gmail.com>
2021-06-30 23:10:28 +1000

    applied font, added comments

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -11,13 +11,25 @@ export function MonoText(props: TextProps) {
   );
 }
 
+/**
+ * Body text node, wraps the default `Text`, use either this or
+ * `HeadingText` instead of React Native's default `Text` for consistent
+ * styling. Accepts additional props to pass down (like inheritance in CSS).
+ * @see https://medium.com/@fullsour/style-inheritance-of-react-native-eca1c974f02b
+ */
 export function BodyText(props: TextProps) {
   return (
     <StyledText {...props} style={[props.style, { fontFamily: 'roboto' }]} />
   );
 }
 
-export function HeaderText(props: TextProps) {
+/**
+ * Heading text node, wraps the default `Text`, use either this or
+ * `BodyText` instead of React Native's default `Text` for consistent
+ * styling. Accepts additional props to pass down (like inheritance in CSS).
+ * @see https://medium.com/@fullsour/style-inheritance-of-react-native-eca1c974f02b
+ */
+export function HeadingText(props: TextProps) {
   return (
     <StyledText {...props} style={[props.style, { fontFamily: 'lato' }]} />
   );
--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,6 +1,7 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { HeadingText, BodyText } from '../components/StyledText';
 
 import { RootStackParamList } from '../types';
 
@@ -9,9 +10,9 @@ export default function NotFoundScreen({
 }: StackScreenProps<RootStackParamList, 'NotFound'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
+      <HeadingText style={styles.title}>This screen doesn't exist.</HeadingText>
       <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
+        <BodyText style={styles.linkText}>Go to home screen!</BodyText>
       </TouchableOpacity>
     </View>
   );
